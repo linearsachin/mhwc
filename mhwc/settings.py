2d48339f13d9eda32ee546cf9a15e8cb0398e166
SacHin Yadav <sachineg39@gmail.com>
2020-06-16 23:55:43 +0530

    bug fixes

--- mhwc/settings.py
+++ mhwc/settings.py
@@ -29,7 +29,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['MHWC_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'peer-space.herokuapp.com']
 
