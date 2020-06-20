3fa2811ad76d0f29713adf53b7a43e319ac466d6
SacHin Yadav <sachineg39@gmail.com>
2020-06-20 13:47:17 +0530

    charcter length increse

--- qna/forms.py
+++ qna/forms.py
@@ -3,26 +3,26 @@ from django.conf import settings
 import datetime
 
 class QuestionSubmission(forms.Form):
-    text = forms.CharField(max_length=500,label =False, required=True,widget=forms.Textarea(attrs={
+    text = forms.CharField(max_length=1000,label =False, required=True,widget=forms.Textarea(attrs={
         'class':"form-control",
         'type':"text",
         'id':"question", 
         'name':"question" ,
         'placeholder':"Ask Anything" ,
-        'rows':2,
+        'rows':10,
         'cols':6,
         'style':"width:70%; text-align-last: center;margin:auto; "
     }))
 
 
 class ReplySubmission(forms.Form):
-    text = forms.CharField(max_length=500,label = False , required=True,widget=forms.Textarea(attrs={
+    text = forms.CharField(max_length=1000,label = False , required=True,widget=forms.Textarea(attrs={
         'class':"form-control",
         'type':"text",
         'id':"reply", 
         'name':"reply" ,
         'placeholder':"Answer" ,
-        'rows':2,
+        'rows':10,
         'cols':6,
         'style':"width:70%; text-align-last: center;margin:auto; "
     }))
