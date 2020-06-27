eb34e2750703dbe7dae5cedbb40e89ee2bb2a2da
SacHin Yadav <sachineg39@gmail.com>
2020-06-28 01:28:16 +0530

    recaptcha

--- qna/models.py
+++ qna/models.py
@@ -33,7 +33,7 @@ class PublicQuestion(models.Model):
     slug = models.SlugField(max_length=50)
     time = models.DateTimeField()
     is_approved = models.BooleanField(default=False)
-    ip = models.GenericIPAddressField(blank=True, null=True)
+    ip = models.GenericIPAddressField(max_length=20,blank=True, null=True)
     remo = models.CharField(blank=True, null=True)
 
     def __str__(self):
