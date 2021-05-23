48679051d2b870109572ed4a6f6874bf46bf7b6a
SacHin Yadav <sachineg39@gmail.com>
2021-05-23 13:22:47 +0530

    qoutes update

--- scrapeqouets.py
+++ scrapeqouets.py
@@ -14,25 +14,24 @@ allP = allNews.find_all('ul')
 print(len(allP))
 
 date = datetime.date.today()+datetime.timedelta(days=-1)
-print(date)
-# for p in allP:
-#     try:
-#         with open('qoutes1.csv', mode='a',encoding='utf-8') as f:
-#             qoutes_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-#             date+= datetime.timedelta(days=1)
-#             if p.text!='':
-#                 qoutes_writer.writerow([p.text,date])
-#     except:
-#         print("error")
+for p in allP:
+    try:
+        with open('qoutes1.csv', mode='a',encoding='utf-8') as f:
+            qoutes_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            date+= datetime.timedelta(days=1)
+            if p.text!='':
+                qoutes_writer.writerow([p.text,date])
+    except:
+        print("error")
 
 
-with open('qoutes1.csv',mode='r',encoding='UTF-8') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    date =str(datetime.date.today())
-    print(date)
-    for index,row in enumerate(csv_reader):
-        # print(row[1])
-        if row[1] == date:
-            print(index,row[0])
+# with open('qoutes1.csv',mode='r',encoding='UTF-8') as csv_file:
+#     csv_reader = csv.reader(csv_file, delimiter=',')
+#     line_count = 0
+#     date =str(datetime.date.today())
+#     print(date)
+#     for index,row in enumerate(csv_reader):
+#         # print(row[1])
+#         if row[1] == date:
+#             print(index,row[0])
         
