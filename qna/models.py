c6c9186bb7163f734af34010964111c931392e2b
SacHin Yadav <sachineg39@gmail.com>
2021-06-24 20:45:01 +0530

    date-format changes

--- qna/models.py
+++ qna/models.py
@@ -38,49 +38,8 @@ class PublicQuestion(models.Model):
     def __str__(self):
         return str(self.question_text)[:20]+"..."
     def get_time_diff(self):
-        if self.time:
-            now = tz.now()
-            timediff = now - self.time
-            hours =  round(timediff.total_seconds()//3600)
-            if hours < 1:
-                return "now"
-            elif hours < 2 and hours >= 1:
-                return "1 hour ago"
-            elif hours >= 2 and hours < 24:
-                return "{hours} hours ago".format(hours=hours)
-            elif hours >= 24 and hours < 48 :
-                return "{days} day ago".format(days=round(hours/24))
-            elif hours >=48:
-                return "{days} days ago".format(days=round(hours/24))
-            else:
-                return ""
+        return self.time.strftime("%b %d, %Y at %H:%M:%S")
             
-
-# class Reply(models.Model):
-#     question = models.ForeignKey(Question,on_delete=models.CASCADE,blank=True, null=True)
-#     reply_text = models.TextField()
-#     time = models.DateTimeField()
-
-#     def __str__(self):
-#         return str(self.reply_text)[:20]+"..."
-#     def get_time_diff(self):
-#         if self.time:
-#             now = tz.now()
-#             timediff = now - self.time
-#             hours =  round(timediff.total_seconds()//3600)
-#             if hours < 1:
-#                 return "now"
-#             elif hours < 2 and hours >= 1:
-#                 return "1 hour ago"
-#             elif hours >= 2 and hours < 24:
-#                 return "{hours} hours ago".format(hours=hours)
-#             elif hours >= 24 and hours < 48 :
-#                 return "{days} day ago".format(days=round(hours/24))
-#             elif hours >=48:
-#                 return "{days} days ago".format(days=round(hours/24))
-#             else:
-#                 return ""
-
 class PublicReply(models.Model):
     question = models.ForeignKey(PublicQuestion,on_delete=models.CASCADE,blank=True, null=True)
     reply_text = models.TextField()
@@ -90,22 +49,7 @@ class PublicReply(models.Model):
     def __str__(self):
         return str(self.reply_text)[:20]+"..."
     def get_time_diff(self):
-        if self.time:
-            now = tz.now()
-            timediff = now - self.time
-            hours =  round(timediff.total_seconds()//3600)
-            if hours < 1:
-                return "now"
-            elif hours < 2 and hours >= 1:
-                return "1 hour ago"
-            elif hours >= 2 and hours < 24:
-                return "{hours} hours ago".format(hours=hours)
-            elif hours >= 24 and hours < 48 :
-                return "{days} day ago".format(days=round(hours/24))
-            elif hours >=48:
-                return "{days} days ago".format(days=round(hours/24))
-            else:
-                return ""
+        return self.time.strftime("%b %d, %Y at %H:%M:%S")
 
 class RepliesReply(models.Model):
     question = models.ForeignKey(PublicReply,on_delete=models.CASCADE,blank=True, null=True)
@@ -116,22 +60,8 @@ class RepliesReply(models.Model):
     def __str__(self):
         return str(self.reply_text)[:20]+"..."
     def get_time_diff(self):
-        if self.time:
-            now = tz.now()
-            timediff = now - self.time
-            hours =  round(timediff.total_seconds()//3600)
-            if hours < 1:
-                return "now"
-            elif hours < 2 and hours >= 1:
-                return "1 hour ago"
-            elif hours >= 2 and hours < 24:
-                return "{hours} hours ago".format(hours=hours)
-            elif hours >= 24 and hours < 48 :
-                return "{days} day ago".format(days=round(hours/24))
-            elif hours >=48:
-                return "{days} days ago".format(days=round(hours/24))
-            else:
-                return ""
+        return self.time.strftime("%b %d, %Y at %H:%M:%S")
+            
 
 
 class BlogLink(models.Model):
@@ -145,7 +75,7 @@ class BlogLink(models.Model):
     def __str__(self):
         return str(self.title)
     def get_date(self):
-        return "{month} {day}, {year}".format(month=self.time.strftime('%B'),day=self.time.day,year=self.time.year)
+        return self.time.strftime("%b %d, %Y")
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
@@ -162,7 +92,7 @@ class Blog(models.Model):
     def __str__(self):
         return str(self.title)
     def get_date(self):
-        return "{month} {day}, {year}".format(month=self.date.strftime('%B'),day=self.date.day,year=self.date.year)
+        return self.time.strftime("%b %d, %Y")
 
 class MoodQ(models.Model):
     q = models.CharField(max_length=80)
